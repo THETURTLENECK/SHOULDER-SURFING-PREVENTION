b8cb44ab476fc6da021f0a3147174d3d22621d2d
DIVYANSHU SRIVASTAVA <106016011+THETURTLENECK@users.noreply.github.com>
2022-09-26 22:25:47 +0530

    Add files via upload

---
+++ JS/javascript.js
@@ -0,0 +1,103 @@
+let display = document.getElementById("password");
+let hidden = document.getElementById("ACTpassword");
+let tempTxt = "";
+let actPASS = "";
+
+document.getElementById("sq1").onclick = () => 
+{
+    display.value = tempTxt + "0";
+    tempTxt = tempTxt + "0";
+
+    hidden.value = actPASS + "A1";
+    actPASS = actPASS + "A1";
+
+    document.getElementById("password").focus();
+};
+
+document.getElementById("sq2").onclick = () => 
+{
+    display.value = tempTxt + "0";
+    tempTxt = tempTxt + "0";
+    
+    hidden.value = actPASS + "A2";
+    actPASS = actPASS + "A2";
+    
+    document.getElementById("password").focus();
+};
+
+document.getElementById("sq3").onclick = () => 
+{
+    display.value = tempTxt + "0";
+    tempTxt = tempTxt + "0";
+
+    hidden.value = actPASS + "A3";
+    actPASS = actPASS + "A3";
+    
+    document.getElementById("password").focus();
+};
+
+document.getElementById("sq4").onclick = () => 
+{
+    display.value = tempTxt + "0";
+    tempTxt = tempTxt + "0";
+    
+    hidden.value = actPASS + "C4";
+    actPASS = actPASS + "C4";
+    
+    document.getElementById("password").focus();
+};
+
+document.getElementById("sq5").onclick = () => 
+{
+    display.value = tempTxt + "0";
+    tempTxt = tempTxt + "0";
+    
+    hidden.value = actPASS + "B5";
+    actPASS = actPASS + "B5";
+    
+    document.getElementById("password").focus();
+};
+
+document.getElementById("sq6").onclick = () => 
+{
+    display.value = tempTxt + "0";
+    tempTxt = tempTxt + "0";
+    
+    hidden.value = actPASS + "C6";
+    actPASS = actPASS + "C6";
+    
+    document.getElementById("password").focus();
+};
+
+document.getElementById("sq7").onclick = () => 
+{
+    display.value = tempTxt + "0";
+    tempTxt = tempTxt + "0";
+    
+    hidden.value = actPASS + "B7";
+    actPASS = actPASS + "B7";
+    
+    document.getElementById("password").focus();
+};
+
+document.getElementById("sq8").onclick = () => 
+{
+    display.value = tempTxt + "0";
+    tempTxt = tempTxt + "0";
+    
+    hidden.value = actPASS + "C8";
+    actPASS = actPASS + "C8";
+    
+    document.getElementById("password").focus();
+};
+
+document.getElementById("sq9").onclick = () => 
+{
+    display.value = tempTxt + "0";
+    tempTxt = tempTxt + "0";
+    
+    hidden.value = actPASS + "B9";
+    actPASS = actPASS + "B9";
+    
+    document.getElementById("password").focus();
+};
